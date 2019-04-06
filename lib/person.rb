21e86fac000500b8a081bc3f70e9321d690e5410
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-06 20:43:06 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+
+@@ -0,0 +1,20 @@
+class Dog
+
+   def name=(name)
+    @name = name
+  end
+
+   def name
+    @name
+  end
+
+   def job=(job)
+    @job = job
+  end
+
+   def job
+    @job
+  end
+end
