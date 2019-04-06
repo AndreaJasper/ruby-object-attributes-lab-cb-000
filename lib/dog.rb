5b300471bafe2b4ef3aa037ca15ee5b40ad86f25
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-06 20:37:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,5 +8,23 @@ class Dog
   end
 end
 
+class Dog
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name=(name)
+    name = name.split
+    @name = name
+  end
+ 
+  def name
+    "#{@name}".strip
+  end
+ 
+end
+
+
 fido = dog.new
 fido.name
